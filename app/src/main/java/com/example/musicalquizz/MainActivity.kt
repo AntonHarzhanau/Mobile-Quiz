b90f3a0dc82cb27446c4d7ffa2c85b03950022a9
Anton Harzhanau <anton.harzhanau@etu.unistra.fr>
2025-05-04 18:13:32 +0200

    added bottom navigation menu

--- app/src/main/java/com/example/musicalquizz/MainActivity.kt
+++ app/src/main/java/com/example/musicalquizz/MainActivity.kt
@@ -5,16 +5,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.musicalquizz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding.botNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.btn_nav_home -> {
+                    binding.tv.text = "Home"
+                }
+                R.id.btn_nav_search -> {
+                    binding.tv.text = "Search"
+                }
+                R.id.btn_nav_settings -> {
+                    binding.tv.text = "Settings"
+                }
+                R.id.btn_nav_playlist -> {
+                    binding.tv.text = "Playlist"
+                }
+            }
+            true
         }
     }
 }
